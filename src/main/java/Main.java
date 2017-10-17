716d44861ba63ca24b836e1e595dc0f213efac96
andrew <andrew@zipcoders-MacBook-Pro.local>
2017-10-17 18:19:38 -0400

    Alice and Bob Greeting Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args ){
-
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        checkName person = new checkName(scanner.nextLine());
+        person.answer();
+        System.out.println(person.getResult());
     }
 }
+
+
