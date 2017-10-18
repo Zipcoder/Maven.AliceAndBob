dc8d156adbca0150147290455fbdbdf98eeeed67
graham <graham@zipcoders-MacBook-Pro.local>
2017-10-18 12:45:15 -0400

    Alice and Bob program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+package com.company;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        System.out.println("Enter name:");
 
+        Scanner nameInput = new Scanner(System.in);
+        String name = nameInput.nextLine();
+
+        if ("Alice".equals(name)){
+
+            System.out.println("Hello!");
+
+        }
+
+        else if("Bob".equals(name)){
+
+            System.out.println("Hello!");
+
+        }
     }
 }
