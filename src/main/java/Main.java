2bcd92c4e98c58044cf43680e3b2c6cfd44de498
abrar <abrar@zipcoders-MacBook-Pro.local>
2017-10-18 16:16:32 -0400

    Added all changed files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert your name: ");
+        String input = scanner.nextLine();
+
+        if ("Bob".equalsIgnoreCase(input)|| "Alice".equalsIgnoreCase(input))
+            System.out.println("Hello " + input);
+        else
+            System.out.println ("Sorry you are not invited.");
 
     }
 }
