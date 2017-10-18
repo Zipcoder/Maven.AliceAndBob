2b7e49e98565e652bed1605008563eddae18c025
ryan <ryan@zipcoders-MacBook-Pro.local>
2017-10-18 13:12:06 -0400

    Finished Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
-
     public static void main(String[] args ){
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name =  scanner.nextLine();
+
+        if("Alice".equals(name) || "Bob".equals(name)) {
+            System.out.println("Hello " + name);
+        }
     }
 }
