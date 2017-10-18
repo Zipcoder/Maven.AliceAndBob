1cd95fdb4d866748642ef084d9682dbdb8d0ceb3
aaron <aaron@zipcoders-MacBook-Pro.local>
2017-10-18 01:17:16 -0400

    Program to greet Alice or Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,37 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+package com.company;
+
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+
+        //Request user to input first name
+        System.out.println("Hello, what is your first name?");
+
+        //Create array for later storing input names
+        ArrayList<String> name = new ArrayList<String>;
+
+        //Create scanner to check user input then add to aforementioned array
+        Scanner userCheck = new Scanner(System.in);
+        userCheck.nextLine().add(name);
+
+        //If name stored in array is "Alice" then greet user
+        If(name = "Alice") {
+            System.out.println("Greetings, Alice!!!");
+        }
+        
+        //If name stored in array is "Bob" then greet user
+        else if(name = "Bob") {
+            System.out.println("Greetings, Bob!!!");
+
+        }
+        
+        //If name stored in array is anything else then do not greet user
+        else() {
+            System.out.println("I have nothing to say to you!");
+        }
 
     }
+
 }
