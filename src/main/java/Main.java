b64ccbeeea1db61d5d0b044cbd579663f9c268eb
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-10-18 12:28:53 -0400

    completed Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
-public class Main {
 
-    public static void main(String[] args ){
+import java.util.Scanner;
 
+    public class Main {
+
+        public static void main (String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            //ask Alie and Bob for their names
+            System.out.println("Please print out your name");
+            String name = sc.nextLine();
+
+            //put in array/ can then uppercase first letter? necessary?
+            //conditional below not working
+
+            if ("Alice".equals(name) || "Bob".equals(name)) {
+                System.out.println("Hello my name is " + name + " ");
+            }
+        }
     }
-}
