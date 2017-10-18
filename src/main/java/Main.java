75779f3bea0d3d647e6554d503ae7d600aac07f0
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-10-18 12:46:29 -0400

    Modify method to say hello to Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
+ * Edits by katherine on 10/18/17
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String yourName = scan.nextLine();
+
+        if("Alice".equals(yourName) || "Bob".equals(yourName)) {
+            System.out.println("Hello " + yourName);
+        }
 
     }
 }
