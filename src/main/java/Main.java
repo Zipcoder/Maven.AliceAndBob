5495157992efe797a94d5f30b889e29f8051ee2d
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-10-18 08:54:09 -0400

    COmplete lab greeting Alice and Bob, ignoring others

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+        String name;
 
+        System.out.print("Please enter your name: ");
+        name=in.nextLine();
+
+        if (name.equalsIgnoreCase("Bob") || name.equalsIgnoreCase("Alice"))
+        {
+            System.out.println("Hello "+name);
+        }
     }
 }
