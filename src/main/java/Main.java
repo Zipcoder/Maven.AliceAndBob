e87d7a32617727674ea45ba18ac2b938ee4481d8
linda <linda@zipcoders-MacBook-Pro-5.local>
2017-10-18 16:52:16 -0400

    alice bob name only

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
 
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if("Alice".equals(input) || "Bob".equals(input)){
+            System.out.println ("Hello " + input);
+
+
+
+        }
     }
 }
