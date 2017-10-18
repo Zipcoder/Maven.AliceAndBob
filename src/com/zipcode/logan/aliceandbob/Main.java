73b229467a989841a6c4ff9c99d089cdaa6b407b
logan <logan@zipcoders-MacBook-Pro.local>
2017-10-18 10:28:52 -0400

    Modified class files

--- src/com/zipcode/logan/aliceandbob/Main.java
+++ src/com/zipcode/logan/aliceandbob/Main.java
@@ -8,12 +8,9 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Please enter your name: ");
         Person user = new Person(keyboard.nextLine());
-        //String name = keyboard.nextLine();
 
         if(user.isBobOrAlice()) {
             user.capitalizeFirst();
-            //name = name.substring(0,1).toUpperCase() + name.substring(1);
-            //name = name.replaceFirst("\b[ab]", name.substring(0,1).toUpperCase());
             System.out.print("Greetings " + user.getName());
         }
         else {
