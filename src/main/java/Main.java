233fbae525cb350bbd08e4ccd6a26f06037a6522
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-2.local>
2018-02-07 00:42:44 -0500

    submission

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.*;
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
+        Scanner input = new Scanner(System.in);
+            System.out.println(" Please write your name!");
+            String name = input.nextLine();
+            if (name.equals("Alice") || name.equals("Bob")) {
+                System.out.println("Hello " + name);
+            }
 
-    }
+        }
+
 }
