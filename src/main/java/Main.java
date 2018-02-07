ffc31bc1006984d5e8bc6b9af709e5a9c9f27433
Lawrence Wu <lawrencewu@zipcoders-MacBook-Pro-3.local>
2018-02-07 09:01:11 -0500

    Alice Bob scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = in.next();
+        if (name.equals("Alice") || name.equals("Bob")){
+            System.out.println("Hello " + name);
+        }
+        else {
+            System.out.println("Please put in Alice or Bob");
+        }
 
     }
 }
