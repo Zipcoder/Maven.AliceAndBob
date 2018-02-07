8750537bd8ee8a631b565d3335d6e3d359e0f290
Dylan Camac <dylancamac@zipcoders-MacBook-Pro-5.local>
2018-02-07 13:21:28 -0500

    save1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
 public class Main {
-// cool comment dude
+// my first comment
     public static void main(String[] args ){
 
-    }
+ Scanner in = new Scanner(System.in);
+
+// get first input here
+        System.out.println("What is your name?");
+        String userName = in.nextLine();
+
+        if (userName.equals("Bob") || userName.equals("Alice")) {
+            System.out.println("Hello, " + userName);
+        } else (userName.equals()) {
+                System.out.println("You are not my friend.");
+                }
+
+
+}
 }
