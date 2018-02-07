96a9f4bceb53960d0f8bde1390c7004086f0e624
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-07 07:56:20 -0500

    here is my code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner AliceBob = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String person = AliceBob.nextLine();
+        if (person.equals("Alice")) {
+            System.out.println("Hello" + AliceBob);
+        } else if (person.equals("Bob")){
+            System.out.println("Hello" + AliceBob);
+        }
+
 
     }
 }
