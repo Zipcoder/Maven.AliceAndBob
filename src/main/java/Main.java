5c7133e73ea4e348b677ee2c0f409395a9320e1b
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-07 08:43:26 -0500

    git commit -m 'got it'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args ){
         Scanner in = new Scanner(System.in);
         String name = in.nextLine();
-        String user1 = "Alice";
-        String user2 = "Bob";
+       // String user1 = "Alice";
+        // String user2 = "Bob";
 
-        if (user1 == "Alice" || user2 == "Bob"){
+        if (name.equals("Alice") || name.equals("Bob")){
             System.out.println("Hey!!");
         } else {
             System.out.println("Try Again!");
