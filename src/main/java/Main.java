921e350e90e375b565ddeff7a3928ce1a50cca98
Anthony Jordan <anthonyjordan@zipcoders-MacBook-Pro-10.local>
2018-02-06 23:02:35 -0500

    Created program that greets Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner name = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String nameInput = name.nextLine();
+        if (nameInput.equalsIgnoreCase("Alice" )| nameInput.equalsIgnoreCase( "Bob")) {
+            System.out.println("Hello " + nameInput);
+        }
 
     }
 }
