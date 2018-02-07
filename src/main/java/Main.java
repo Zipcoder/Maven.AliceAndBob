781513ee26c726166be46d8e5125cba345137ebf
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-07 01:56:24 -0500

    git commit -m 'All I do is Win'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        String user1 = "Alice";
+        String user2 = "Bob";
+
+        if (user1 == "Alice" || user2 == "Bob"){
+            System.out.println("Hey!!");
+        } else {
+            System.out.println("Try Again!");
+        }
 
     }
 }
