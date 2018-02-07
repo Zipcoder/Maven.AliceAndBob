e13f15c15c913da3f8244a4df8962da2ec917eca
Sean Mis <seanmis@MacBook-Pro-3.local>
2018-02-07 07:34:43 -0500

    Alice and Bob first try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name.");
+        String input = sc.nextLine();
+        if (input.equals("Alice")) {
+            System.out.println("Hello Alice!");
+        } else if (input.equals("Bob")) {
+            System.out.println("Hello Bob!");
+        }
     }
 }
