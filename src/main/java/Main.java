ba01ee6c09268f957853b2525d948672d737b2f0
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-07 15:33:25 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,14 @@ public class Main {
 
     public static void main(String[] args ){
 
+        Scanner myTest = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String username = myTest.nextLine();
+
+        if (username.equalsIgnoreCase("Alice") || username.equalsIgnoreCase("Bob")) {
+            System.out.println("Hello " + username);
+        } else {
+                System.out.println("Go away!");
+            }
+        }
     }
-}
