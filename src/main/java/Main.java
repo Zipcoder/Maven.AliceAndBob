f1e2021ee9dbb77ea14b80ecb7f9ae13e73d956f
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-3.local>
2018-02-06 23:50:36 -0500

    Bob and Alice are lucky to be greeted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+
+        Scanner sc = new Scanner(System.in);
+        String usersName;
+
+        System.out.println("Hello! Please enter your name: ");
+        usersName = sc.nextLine();
+
+       if( usersName.equals("Alice") || usersName.equals("Bob")){
+           System.out.println("Hello, " + usersName + ", nice to meet you!");
+       }
 
     }
 }
