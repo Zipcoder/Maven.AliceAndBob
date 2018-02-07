7e7a1f0860643eb7ae572a45365c35ac9b74fff1
Madeline Bowe <madelinebowe@zipcoders-MacBook-Pro-2.local>
2018-02-07 09:25:56 -0500

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+
+        String userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
 
     }
 }
