3a5683f1201eb281ca724246bcd91efd12bd8593
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-07 08:07:24 -0500

    Please Work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
 
+        System.out.println("Please enter your name:");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        if (name.equals("Alice") || name.equals("Bob")) {
+            System.out.println("Hello " + name + "!");
+        } else {
+            System.out.println("Who are you?  Go away!");
+        }
+
     }
+
 }
