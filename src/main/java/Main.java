b624c91bf99c951b7f919419c83c8a7b31a3d906
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-02-07 10:49:18 -0500

    One issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import  java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("What is your name?");
+
+        String input = in.nextLine();
+
+        if (input == "Alice") {
+            System.out.println("Hello " + input);
+        } else if (input == "Bob") {
+            System.out.println("Hello " + input);
+        } else {
+            System.out.println("Thats not my name");
+        }
     }
-}
