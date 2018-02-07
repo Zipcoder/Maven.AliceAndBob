830c08e4b1c2274e4df2a33cb9e06b09aa8935ee
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-02-07 15:14:06 -0500

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
+;
 
+        Scanner nameGame = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String userJazz = nameGame.nextLine();
+
+        if (userJazz.equals("Alice") || userJazz.equals("Bob")) {
+            System.out.println("Hello " + userJazz + "!");
+        } else {
+            System.out.println("I don't know you fool!");
+        }
     }
 }
