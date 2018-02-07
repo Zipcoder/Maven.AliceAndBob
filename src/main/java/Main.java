ce33c040b3eddde110d79f0e5c34837bf8e93db3
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-02-06 22:15:13 -0500

    Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.println("What is your name?");
+        name = scanner.next();
 
+        if (name.toLowerCase().equals("bob") || name.toLowerCase().equals("alice")){
+            System.out.println("Hi!");
+        }
+
+        else {
+            System.out.println("You're not Bob or Alice");
+        }
     }
+
 }
