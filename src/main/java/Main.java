108b66fb9643c505b480b3c336a4b4f8b298b2a9
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-07 08:45:02 -0500

    updated Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+
+        if (name.equals("Bob") || name.equals("Alice")) {
+            System.out.println("Hello " + name);
+        } else {
+            System.out.println("This place is not for you.  Go away.");
+        }
+
+
 
     }
 }
