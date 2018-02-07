03801d79245ea6d04f276d961b70ce23fd73caee
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-07 09:03:58 -0500

    Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.*;
 public class Main {
 
     public static void main(String[] args ){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Are you Alice or Bob?");
+
+        String name = scan.nextLine();
+
+        if(name.equals("Alice")) {
+            System.out.println("Hi Alice");
+        } else if( name.equals("Bob")) {
+            System.out.println("Hi Bob");
+        } else {
+            System.out.println("Leon? Is that you?");
+        }
 
     }
 }
