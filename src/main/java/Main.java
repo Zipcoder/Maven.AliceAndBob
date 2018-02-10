9db3689b9bbe3cc39f17036509d090c100de191e
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-10 17:24:47 -0500

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+//redone with instructor help
 public class Main {
 
     public static void main(String[] args ){
@@ -9,12 +11,24 @@ public class Main {
         String name = in.nextLine();
        // String user1 = "Alice";
         // String user2 = "Bob";
-
-        if (name.equals("Alice") || name.equals("Bob")){
+        /*  if (name.equals("Alice") || name.equals("Bob")){
             System.out.println("Hey!!");
         } else {
             System.out.println("Try Again!");
+        }*/
+
+    testingNames( "Alice", name);
+    testingNames( "Bob", name);
+    }
+
+
+    //testingNames is your method
+    public static void testingNames(String username, String enteredname){
+        //this is basically saying if the entered name by the user is equal to the username: "Alice" or "Bob" then print
+        if (enteredname.equals(username)){
+            System.out.println("Hey!!!");
         }
+
 
     }
 }
