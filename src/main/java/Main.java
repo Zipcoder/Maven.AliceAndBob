4993eb3878914b4b0e40593e004d74a7145b32a4
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-19 11:02:42 -0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args ) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hi there! What's your name?");
+        String name = input.nextLine();
+
+        if (isMatch(name)) {
+            System.out.println("Hi " + name);
+        }
+
+    }
+
+    public static boolean isMatch(String name) {
+
+        if (name.toLowerCase().equals("bob") || name.toLowerCase().equals("alice")) {
+            return true;
+        }
+
+        return false;
     }
 }
